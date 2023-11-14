7e9d61acbb3ba32b4520462b145279b16472285e
Sebastian Zuviria <szuviria@gmail.com>
2023-11-13 22:56:18 -0300

    Clase de jsx y empaquetadores

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
         <nav className={classes.container} style={{ display: 'flex', justifyContent: 'space-between'}}>
             <h1 className={classes.rojo}>Ecommerce</h1>
             <section className={classes.categories}>
-                <button>Celulares</button>
-                <button>Tablets</button>
-                <button>Notebook</button>
+                <button className='btn btn-success'>Celulares</button>
+                <button className='btn btn-danger'>Tablets</button>
+                <button className='btn btn-warning'>Notebook</button>
             </section>
         </nav>
     )
